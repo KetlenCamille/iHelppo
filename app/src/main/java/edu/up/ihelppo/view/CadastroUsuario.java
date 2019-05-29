c776fabc5b27eecf5e2eb8d9c4277e83d1416676
Ricardo Ferreira <ricardo.ferreirap@hotmail.com>
2019-05-28 23:44:09 -0300

    Criado tela inicial de login e de cadastro

---
+++ app/src/main/java/edu/up/ihelppo/view/CadastroUsuario.java
@@ -0,0 +1,26 @@
+package edu.up.ihelppo.view;
+
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+import edu.up.ihelppo.R;
+
+public class CadastroUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_usuario);
+
+    }
+}

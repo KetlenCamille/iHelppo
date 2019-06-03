d3b5268e3e191ffe30f0b6aaf0855f0e791739a0
KetlenCamille <ketlen.rieper@gmail.com>
2019-06-03 01:10:10 -0300

    Model Usuario e Criando o método CadastrarUsuario

---
+++ app/src/main/java/edu/up/ihelppo/model/Usuario.java
@@ -0,0 +1,61 @@
+package edu.up.ihelppo.model;
+
+import java.util.Date;
+
+public class Usuario {
+
+    private int idUsuario;
+    private String nome;
+    private String sobrenome;
+    private String email;
+    private Date dataNascimento;
+    private String senha;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
--- app/src/main/java/edu/up/ihelppo/view/CadastroUsuario.java
+++ app/src/main/java/edu/up/ihelppo/view/CadastroUsuario.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,13 +15,36 @@ import android.widget.TextView;
 import java.util.Calendar;
 
 import edu.up.ihelppo.R;
+import edu.up.ihelppo.model.Usuario;
 
 public class CadastroUsuario extends AppCompatActivity {
+
+    private EditText edtNome, edtSobrenome, edtEmail, edtDataNasc, edtSenha;
+    private Button btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuario);
 
+        edtNome = (EditText) findViewById(R.id.edtNome);
+        edtSobrenome = (EditText) findViewById(R.id.edtSobrenome);
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtDataNasc = (EditText) findViewById(R.id.edtDataNasc);
+        edtSenha = (EditText) findViewById(R.id.edtSenha);
+        btnCadastrar = (Button) findViewById(R.id.btnCadastrar);
+    }
+
+    public void btnCadastrarUsuarioClick(View view) {
+        Usuario usuario = new Usuario();
+        usuario.setNome(edtNome.getText().toString());
+        usuario.setSobrenome(edtSobrenome.getText().toString());
+        usuario.setEmail(edtEmail.getText().toString());
+        //usuario.setDataNascimento(edtDataNasc.getText().toString());
+        usuario.setSenha(edtSenha.getText().toString());
+
+        //Chamar DAO
+
+        //Criar Intent pra Main
     }
 }

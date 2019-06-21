4da85122d915eb60cc39dcd994132cb492455b93
Ricardo Ferreira <ricardo.ferreirap@hotmail.com>
2019-06-21 18:54:35 -0300

    Merge branch 'master' of https://github.com/KetlenCamille/iHelppo
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/layout/activity_cadastrar_atividade.xml
    
     Criado telas

---
+++ app/src/main/java/edu/up/ihelppo/view/DetalhesAtividadeActivity.java
@@ -0,0 +1,15 @@
+package edu.up.ihelppo.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.up.ihelppo.R;
+
+public class DetalhesAtividadeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_atividade);
+    }
+}

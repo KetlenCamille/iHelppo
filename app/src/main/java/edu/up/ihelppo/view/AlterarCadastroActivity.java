e827100d51605c13a3eb450328d22af48a88dc1f
Ricardo Ferreira <ricardo.ferreirap@hotmail.com>
2019-06-21 16:49:55 -0300

    Merge branch 'master' of https://github.com/KetlenCamille/iHelppo
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/edu/up/ihelppo/view/CadastrarAtividadeActivity.java
    #	app/src/main/res/layout/activity_cadastrar_atividade.xml
    
    Criado logo
    Criado tela de cadastro de atividade

---
+++ app/src/main/java/edu/up/ihelppo/view/AlterarCadastroActivity.java
@@ -0,0 +1,15 @@
+package edu.up.ihelppo.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.up.ihelppo.R;
+
+public class AlterarCadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alterar_cadastro);
+    }
+}
